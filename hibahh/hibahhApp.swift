8a6838c13a4ef01cf68610939718662cbae97bdb
ALWALEED-KHUSHAIM <doode711.cr7@gmail.com>
2023-09-05 13:58:26 +0300

    Initial Commit

---
+++ hibahh/hibahhApp.swift
@@ -0,0 +1,17 @@
+//
+//  hibahhApp.swift
+//  hibahh
+//
+//  Created by الوليد خشيم on 20/02/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct hibahhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
